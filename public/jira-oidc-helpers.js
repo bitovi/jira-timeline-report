e75a7c9c45d246c755375887fd4aa59d6bdaec46
Ayoola Roy-Layinde <ayoolaroy-layinde@Ayoolas-MacBook-Pro.local>
2022-10-25 18:29:21 +0100

    code refactoring

--- public/jira-oidc-helpers.js
+++ public/jira-oidc-helpers.js
@@ -10,43 +10,81 @@ function JiraOIDCHelpers({
 		return response.json();
 	}
 
+	let refreshingRequest = false;
 	const jiraHelpers = {
+
 		saveInformationToLocalStorage: (parameters) => {
 				const objectKeys = Object.keys(parameters)
 				for(let key of objectKeys) {
 						window.localStorage.setItem(key, parameters[key]);
 				}
 		},
+
 		fetchFromLocalStorage: (key) => {
 				return window.localStorage.getItem(key);
 		},
+
 		fetchAuthorizationCode: () => {
 				const url = `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${JIRA_CLIENT_ID}&scope=${JIRA_SCOPE}&redirect_uri=${JIRA_CALLBACK_URL}&response_type=code&prompt=consent`;
 				window.location.href = url;
 		},
-		fetchableRefresher: async (url, method = 'GET', body = undefined) => {
-			if(!jiraHelpers.hasValidAccessToken){
-				await jiraHelpers.refreshAccessToken();
+
+		hasValidAccessToken: () => {
+			const accessToken = jiraHelpers.fetchFromLocalStorage("accessToken");
+			let expiryTimestamp = Number(jiraHelpers.fetchFromLocalStorage("expiryTimestamp"));
+			if(isNaN(expiryTimestamp)) {
+				expiryTimestamp = 0;
 			}
-			const shouldRefresh = jiraHelpers.isValidForRefresh();
+			const currentTimestamp = Math.floor(new Date().getTime()/1000.0);
+			return !((currentTimestamp > expiryTimestamp) || (!accessToken));
+		},
+
+		getAccessToken: async () => {
+			if( !jiraHelpers.hasValidAccessToken() ){
+				const refreshToken = jiraHelpers.fetchFromLocalStorage("refreshToken");
+				if(!refreshToken) {
+					jiraHelpers.fetchAuthorizationCode();
+				}else{
+					return jiraHelpers.refreshAccessToken();
+				}
+			} else {
+				return jiraHelpers.fetchFromLocalStorage("accessToken");
+			}
+		},
+
+		refreshableFetch: async (url, options) => {
+			if(!jiraHelpers.hasValidAccessToken) {
+				if(!refreshingRequest) {
+					refreshingRequest = jiraHelpers.refreshAccessToken;
+					await refreshingRequest();
+					refreshingRequest = null;
+				}
+			}
 			const accessToken = jiraHelpers.fetchFromLocalStorage("accessToken");
 			const config = {
+				...options,
 				headers: new Headers({
 					Authorization: `Bearer ${accessToken}`
 				}),
-				method,
 			}
-			if(shouldRefresh) {
-				setTimeout(() => {
-					jiraHelpers.refreshAccessToken();
-				}, 10000);
-			}
-			return fetch(url, config).then(responseToJSON);
+			return fetch(url, config);
 		},
+
+		jiraFetch: async (urlPath, method = 'GET', body = undefined) => {
+			const scopeIdForJira = jiraHelpers.fetchFromLocalStorage('scopeId');
+			const url = `${JIRA_API_URL}/ex/jira/${scopeIdForJira}/${urlPath}`;
+			const options = {
+				method,
+				...(body ? {body: JSON.stringify(body)} : {})
+			};
+			return await jiraHelpers.refreshableFetch(url, options).then(responseToJSON);
+		},
+
 		refreshAccessToken: async (accessCode) => {
 				try {
-						const response = await fetch(`./access-token?code=${accessCode}&refresh=true`)
-						if(!response.ok) throw new Error("")
+						const refreshCode = jiraHelpers.fetchFromLocalStorage('refreshToken');						
+						const response = await fetch(`./access-token?code=${refreshCode}&refresh=true`)
+						if(!response.ok) throw new Error("Unable to fetch refresh token")
 						const data = await response.json();
 						const {
 								accessToken,
@@ -60,11 +98,12 @@ function JiraOIDCHelpers({
 						});
 						return accessToken;
 				} catch (error) {
-						console.error(error.message)
-						window.localStorage.clear()
+						console.error(error.message);
+						window.localStorage.clear();
 						jiraHelpers.fetchAuthorizationCode();
 				}
 		},
+
 		fetchAccessTokenWithAuthCode: async (authCode) => {
 				try {
 						const response = await fetch(`./access-token?code=${authCode}`)
@@ -89,141 +128,17 @@ function JiraOIDCHelpers({
 						console.error(error);
 				}
 		},
+
 		fetchJiraIssue: async (issueId) => {
-			const scopeIdForJira = jiraHelpers.fetchFromLocalStorage('scopeId');
-			const url = `${JIRA_API_URL}/ex/jira/${scopeIdForJira}/rest/api/3/issue/${issueId}`;
-			return jiraHelpers.fetchableRefresher(url);
+			const urlPath = `/rest/api/3/issue/${issueId}`;
+			return jiraHelpers.jiraFetch(urlPath);
 		},
-		fetchJiraIssuesWithJQL: function(params){
-			const scopeIdForJira = jiraHelpers.fetchFromLocalStorage('scopeId');
-			const url = `${JIRA_API_URL}/ex/jira/${scopeIdForJira}/rest/api/3/search?` + new URLSearchParams(params);
-			return jiraHelpers.fetchableRefresher(url);
-			
-		},
-		fetchAllJiraIssuesWithJQL: async function(params){
-			const firstRequest = jiraHelpers.fetchJiraIssuesWithJQL({maxResults: 100, ...params});
-			const {issues, maxResults, total, startAt} = await firstRequest;
-			const requests = [firstRequest];
-			for(let i = startAt+maxResults; i < total; i += maxResults) {
-				requests.push(
-					jiraHelpers.fetchJiraIssuesWithJQL({maxResults: maxResults, startAt: i, ...params})
-				);
-			}
-			return Promise.all(requests).then(
-				(responses)=> {
-					return responses.map( (response)=> response.issues ).flat();
-				}
-			)
-		},
-		fetchJiraChangelog(issueIdOrKey, params){
-			const scopeIdForJira = jiraHelpers.fetchFromLocalStorage('scopeId');
-			const url = `${JIRA_API_URL}/ex/jira/${scopeIdForJira}/rest/api/3/issue/${issueIdOrKey}/changelog?` +new URLSearchParams(params)
-			return jiraHelpers.fetchableRefresher(url);
-		},
-		isChangelogComplete(changelog) {
-			return changelog.histories.length === changelog.total
-		},
-		fetchRemainingChangelogsForIssues(issues) {
-			// check for remainings
-			return Promise.all(issues.map( issue => {
-				if(jiraHelpers.isChangelogComplete(issue.changelog)){
-					return {
-						...issue,
-						changelog: issue.changelog.histories
-					}
-				} else {
-					return jiraHelpers.fetchRemainingChangelogsForIssue(issue.key, issue.changelog).then( (histories)=>{
-						return {
-							...issue,
-							changelog: issue.changelog.histories
-						}
-					})
-				}
-			}))
-		},
-		// weirdly, this starts with the oldest, but we got the most recent
-		// returns an array of histories objects
-		fetchRemainingChangelogsForIssue(issueIdOrKey, mostRecentChangeLog) {
 
-			const {histories, maxResults, total, startAt} = mostRecentChangeLog;
-
-			const requests = [];
-			for(let i = 0; i < total - maxResults; i += maxResults) {
-				requests.push(
-					jiraHelpers.fetchJiraChangelog(issueIdOrKey, {
-						maxResults: Math.min(maxResults, total - maxResults - i),
-						startAt: i,
-					})
-				);
-			}
-			// server sends back as "values", we match that
-			requests.push({values: mostRecentChangeLog.histories.reverse()});
-			return Promise.all(requests).then(
-				(responses)=> {
-					return responses.map( (response)=> response.values ).flat();
-				}
-			).then(function(response){
-				return response;
-			})
-		},
-		fetchAllJiraIssuesWithJQLAndFetchAllChangelog: async function(params){
-			function getRemainingChangeLogsForIssues(response) {
-				return jiraHelpers.fetchRemainingChangelogsForIssues(response.issues)
-			}
-
-			const firstRequest = jiraHelpers.fetchJiraIssuesWithJQL({maxResults: 100, expand: ["changelog"], ...params});
-
-			const {issues, maxResults, total, startAt} = await firstRequest;
-			const requests = [firstRequest.then(getRemainingChangeLogsForIssues)];
-
-			for(let i = startAt+maxResults; i < total; i += maxResults) {
-				requests.push(
-					jiraHelpers.fetchJiraIssuesWithJQL({maxResults: maxResults, startAt: i, ...params})
-						.then( getRemainingChangeLogsForIssues )
-				);
-			}
-			return Promise.all(requests).then(
-				(responses)=> {
-					return responses.flat();
-				}
-			)
-		},
 		fetchJiraFields(){
-			const scopeIdForJira = jiraHelpers.fetchFromLocalStorage('scopeId');
-			const url = `${JIRA_API_URL}/ex/jira/${scopeIdForJira}/rest/api/3/field`;
-			return jiraHelpers.fetchableRefresher(url);
+			const url = `/rest/api/3/field`;
+			return jiraHelpers.jiraFetch(url);
 		},
-		getAccessToken: async () => {
-			if( !jiraHelpers.hasValidAccessToken() ){
-				const refreshToken = jiraHelpers.fetchFromLocalStorage("refreshToken");
-				if(!refreshToken) {
-					jiraHelpers.fetchAuthorizationCode();
-				}else{
-					return jiraHelpers.refreshAccessToken();
-				}
-			} else {
-				return jiraHelpers.fetchFromLocalStorage("accessToken");
-			}
-		},
-		hasValidAccessToken: () => {
-			const accessToken = jiraHelpers.fetchFromLocalStorage("accessToken");
-			let expiryTimestamp = Number(jiraHelpers.fetchFromLocalStorage("expiryTimestamp"));
-			if(isNaN(expiryTimestamp)) {
-				expiryTimestamp = 0;
-			}
-			const currentTimestamp = Math.floor(new Date().getTime()/1000.0);
-			return !((currentTimestamp > expiryTimestamp) || (!accessToken))
-		},
-		isValidForRefresh() {
-			let expiryTimestamp = Number(jiraHelpers.fetchFromLocalStorage("expiryTimestamp"));
-			if(isNaN(expiryTimestamp)) {
-				expiryTimestamp = 0;
-			}
-			const currentTimestamp = Math.floor(new Date().getTime()/1000.0);
-			const timeDifference = expiryTimestamp - currentTimestamp;
-			const timeToExpiryForRefresh = 300; //in seconds
-			return timeDifference <= timeToExpiryForRefresh;
-	}
+
 	}
 
 	return jiraHelpers;
--- public/main.js
+++ public/main.js
@@ -4,7 +4,6 @@ async function main(jiraHelpers) {
 	mainElement.textContent = "Checking for Jira Access Token";
 
 	if(!jiraHelpers.hasValidAccessToken()) {
-		await sleep(100);
 		mainElement.textContent = "Getting access token";
 	}
 
@@ -12,57 +11,18 @@ async function main(jiraHelpers) {
 
 	mainElement.textContent = "Got Access Token "+ accessToken;
 
-	const issue = await jiraHelpers.fetchJiraIssue('PICK-ISSUE');
-	console.log(issue);
-
 	const fields = await jiraHelpers.fetchJiraFields();
 
 	mainElement.textContent = "Requesting field definitions";
 
 	const fieldMap = makeFieldNameToIdMap(fields);
-	console.log(fieldMap);
-}
-
-
-function sleep(time){
-	return new Promise((resolve)=>{
-		setTimeout(resolve, time)
-	})
+	mainElement.innerHTML = `Fields: ${JSON.stringify(fieldMap)}`;	
 }
 
 function makeFieldNameToIdMap(fields){
 	const map = {};
-	fields.forEach((f) => {
+	fields.forEach(f => {
 		map[f.name] = f.id;
 	});
 	return map;
-}
-
-function isChangelogComplete(changelog) {
-	return changelog.histories.length === changelog.total
-}
-
-function calculateTimeInStatus(changelog, status){
-	let totalTime = 0;
-	let currentStatus = "To Do";
-	let inDevelopmentTime;
-	changelog.reverse().forEach( (history) => {
-		history.items.forEach( (changeItem)=> {
-			if(changeItem.field === "status") {
-				if( changeItem.toString === "In Development" ) {
-					inDevelopmentTime = new Date(history.created);
-				} else if(changeItem.fromString === "In Development") {
-					if(!inDevelopmentTime) {
-						console.error("No development time!")
-					} else {
-						totalTime += new Date(history.created) - inDevelopmentTime;
-						inDevelopmentTime = undefined;
-					}
-
-				}
-
-			}
-		})
-	})
-	return totalTime;
 }
