9a57a7f29c9596b9f60e4f076e864782c5ec50e7
Justin Meyer <justinbmeyer@gmail.com>
2023-10-24 17:59:53 -0500

    adding errors and logout for #28

--- pages/index.html.js
+++ pages/index.html.js
@@ -27,15 +27,24 @@ module.exports = function (env) {
 					<li>
 						<a href="https://github.com/bitovi/jira-timeline-report" class="color-gray-900 text-2xl underline-on-hover">Jira Timeline Report</a>
 					</li>
+					<li>
+						<a href="https://www.bitovi.com/services/agile-project-management-consulting" class="by-bitovi">Bitovi</a>
+					</li>
 				</ul>
 				<div>
-					<a href="https://www.bitovi.com/services/agile-project-management-consulting" class="by-bitovi">Bitovi</a>
+					<button id="logout" class="p-2 block color-gray-900 underline-on-hover pointer">Log out</button>
 				</div>
 			</div>
 		</nav>
 	</div>
 			<div id="mainElement" class='place-center w-1280'>Loading ... </div>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.2/axios.min.js"></script>
+			<script>
+			logout.onclick = function(){
+				localStorage.clear();
+				window.location.reload();
+			}
+			</script>
 			<script type="module">
 				import JiraOIDCHelpers from "./jira-oidc-helpers.js";
 				import main from "./main.js";
