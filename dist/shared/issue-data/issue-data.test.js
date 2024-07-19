8ebb5f6a9a43501c0809143c92159a8148056eed
Nicole Greene <ngreene4@apple.com>
2024-07-19 13:02:57 -0700

    remove

--- dist/shared/issue-data/issue-data.test.js
+++
@@ -1,7 +0,0 @@
-// sum.test.js
-import { expect, test } from 'vitest'
-import { getConfidenceDefault } from './issue-data.js'
-
-test('getConfidenceDefault', () => {
-  expect(getConfidenceDefault({fields: {Confidence: 20}})).toBe(20);
-})
