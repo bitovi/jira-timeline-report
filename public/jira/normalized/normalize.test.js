f4453022cd6d1fb67664b0a6f75f0ff4fed62f5a
Justin Meyer <justinbmeyer@gmail.com>
2024-07-20 14:44:17 -0500

    getting tests to pass

--- public/jira/normalized/normalize.test.js
+++ public/jira/normalized/normalize.test.js
@@ -1,6 +1,6 @@
 // sum.test.js
 import { expect, test } from 'vitest'
-import { getConfidenceDefault } from './issue-data.js'
+import { getConfidenceDefault } from './normalize.js'
 
 test('getConfidenceDefault', () => {
   expect(getConfidenceDefault({fields: {Confidence: 20}})).toBe(20);
