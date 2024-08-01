5ccbb67754ef9183fe021b260afe3a6c57d25f5c
Nicole Greene <ngreene4@apple.com>
2024-08-01 09:21:07 -0700

    remove comments

--- public/request-helpers/connect-request-helper.js
+++ public/request-helpers/connect-request-helper.js
@@ -3,13 +3,7 @@ import responseToJSON from "../shared/response-to-json";
 async function fetchJSON(url, options) {
 	return fetch(url, options).then(responseToJSON)
 }
-/*https://5580-68-187-209-164.ngrok-free.app/connect?
-xdm_e=https%3A%2F%2Fbmomberger-bitovi.atlassian.net&
-xdm_c=channel-bitovi.timeline-report__main
-cp=
-xdm_deprecated_addon_key_do_not_use=bitovi.timeline-report&
-lic=none
-&cv=1001.0.0-SNAPSHOT*/
+
 export function getConnectRequestHelper() {
   return function(requestUrl) {
     return new Promise(async(resolve, reject) => {
@@ -17,12 +11,7 @@ export function getConnectRequestHelper() {
         let result;
         if(requestUrl.startsWith('https://')) {
           result = await fetchJSON(
-            requestUrl,
-            {
-/*              headers: {
-                'Authorization': `Bearer ${requestJWT}`,
-              }
-*/            }
+            requestUrl, {}
           );
         } else {
           result = JSON.parse((await AP.request(`/rest/${requestUrl}`)).body)
