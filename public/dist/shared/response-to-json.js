678d907c172986326ae3540fa96c752fa780a23f
David Brandon <dbrandon@bitovi.com>
2024-09-18 17:22:58 -0500

    dist update

---
+++ public/dist/shared/response-to-json.js
@@ -0,0 +1,12 @@
+export function responseToJSON(response) {
+    if (!response.ok) {
+        return response.json().then(function (payload) {
+            var err = new Error("HTTP status code: " + response.status);
+            Object.assign(err, payload);
+            Object.assign(err, response);
+            throw err;
+        });
+    }
+    return response.json();
+}
+//# sourceMappingURL=response-to-json.js.map
