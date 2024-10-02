e1d80fc63c3e9829770b479b8ced790a411be70f
Denison Luz <denison.luz@pizzahut.com>
2024-10-02 10:34:24 -0300

    running tests only against Firefox (for now) to avoid CORS errors

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,19 +37,17 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
