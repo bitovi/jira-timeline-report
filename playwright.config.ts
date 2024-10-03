00b6fa13a0738d80df7f954c667597ec918f1fb2
Denison Luz <denison.luz@pizzahut.com>
2024-10-03 11:27:14 -0300

    throw error if an existing web server  process is listening on the url - this is important to make sure server uses correct .env variables

--- playwright.config.ts
+++ playwright.config.ts
@@ -85,7 +85,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI || true,
+    reuseExistingServer: false,
   },
   reportSlowTests: null
 });
