d398e5fcf57167f297d778dd69d269015de490e1
David Nicholas <dnicholas@bitovi.com>
2024-11-26 11:11:08 -0800

    clean up

--- public/jira/reports/fetcher.ts
+++ public/jira/reports/fetcher.ts
@@ -8,21 +8,6 @@ export type Report = {
 
 export type Reports = Partial<Record<string, Report>>;
 
-// export interface Reports extends RawReport {
-//   params: Record<string, string | string[] | undefined>;
-// }
-
-// export function createReportFromQueryString(id: string, queryString: string): Reports {
-//   const searchParams = new URLSearchParams(queryString);
-//   const params = Object.fromEntries(searchParams.entries());
-
-//   return {
-//     id,
-//     stringifiedParams: queryString,
-//     params,
-//   };
-// }
-
 const reportsKey = "saved-reports";
 
 export const getAllReports = (storage: AppStorage): Promise<Reports> => {
