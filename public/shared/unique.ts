3c666108213b8a509c98b0ee7aa7e66cd59015c6
David Brandon <dbrandon@bitovi.com>
2024-12-10 09:07:16 -0600

    addressing PR comments

--- public/shared/unique.ts
+++ public/shared/unique.ts
@@ -1,4 +1,4 @@
-export function unique<TItem, TKey>(list: TItem[], uniqueBy: (list: TItem) => TKey) {
+export function unique<TItem, TKey extends string | number | symbol>(list: TItem[], uniqueBy: (list: TItem) => TKey) {
   return list.filter((item, i, listInner) => {
     const itemKey = uniqueBy(item);
     return listInner.findIndex((itemInner) => uniqueBy(itemInner) === itemKey) === i;
@@ -9,6 +9,6 @@ export function uniqueKeys<TItem extends { key: string }>(list: TItem[]) {
   return unique(list, ({ key }) => key);
 }
 
-export function uniqueIds<TId, TItem extends { id: TId }>(list: TItem[]) {
+export function uniqueIds<TId extends string | number | symbol, TItem extends { id: TId }>(list: TItem[]) {
   return unique(list, ({ id }) => id);
 }
