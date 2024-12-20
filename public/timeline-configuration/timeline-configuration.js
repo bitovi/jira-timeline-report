b8d1ed4db5563b36d82f5f9c275864f87203317f
David Brandon <dbrandon@bitovi.com>
2024-12-20 13:18:20 -0600

    update sidebar help text

--- public/timeline-configuration/timeline-configuration.js
+++ public/timeline-configuration/timeline-configuration.js
@@ -48,7 +48,7 @@ const GOBACK_BUTTON = `
 
 export class TimelineConfiguration extends StacheElement {
   static view = `
-    <div class="px-3 py-2 h-full">
+    <div class="px-3 py-2 h-full min-w-40">
 
         {{# if(this.isPlugin)}}
           <div class="flex gap-2 pt-4">
@@ -91,11 +91,24 @@ export class TimelineConfiguration extends StacheElement {
                     <span class="pl-3">Teams</span>
             </button>
 
-            <p class="fixed bottom-4">
-                <h3 class="font-bold uppercase text-slate-300 text-xs pb-1">Questions? </h3>
-                <a class="link block" href="https://github.com/bitovi/jira-timeline-report/tree/main?tab=readme-ov-file#getting-started">Read the guide</a>
-                <a class="link block" href="https://github.com/bitovi/jira-timeline-report/tree/main?tab=readme-ov-file#need-help-or-have-questions">Connect with us</a>
-            </p>  
+            <div class="fixed bottom-4 grid justify-items-center gap-2 p-1">
+              <img class='pb-2' width="24px" src="./images/_QuestionCircleIcon_.svg"/>
+              <a class='link text-slate-300 text-sm' target="_blank" 
+                href="https://github.com/bitovi/jira-timeline-report/tree/main?tab=readme-ov-file#getting-started"
+              >
+                Read the guide
+              </a>
+              <a class='link text-slate-300 text-sm' target="_blank" 
+                href="https://www.bitovi.com/academy/learn-agile-program-management-with-jira.html"
+              >
+                APM Training
+              </a>
+              <a class='link text-slate-300 text-sm' target="_blank" 
+                href="https://www.bitovi.com/services/agile-project-management-consulting"
+              >
+                Connect with Bitovi
+              </a>
+            </div>
         {{/ not }}
         
         <div width="w-96"  class="{{^ eq(this.showSettings, "SOURCES")}}hidden{{/}}">
