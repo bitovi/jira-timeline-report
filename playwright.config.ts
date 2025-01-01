e172229b7855994627ea4f5cff60614a315d17f6
Elfouih <ilyass.elfouih@gmail.com>
2025-01-01 16:17:47 +0100

    refactor: remove deprecated Playwright setup and teardown files, update test commands, and add e2e test workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -12,8 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './playwright/',
-  outputDir: './playwright/test-results',
+  testDir: "./playwright/",
+  outputDir: "./playwright/test-results",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,33 +23,23 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { outputFolder: './playwright/report' }]],
+  reporter: [["html", { outputFolder: "./playwright/report" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // command: BASE_URL=https://timeline-report.bitovi-jira.com npx playwright test
-    baseURL: process.env.BASE_URL || 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    video: 'on',
+    trace: "on-first-retry",
+    video: "on",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'set-env-variables',
-      testMatch: /global\.setup\.ts/,
-      teardown: 'restore-env-variables',
-    },
-    {
-      name: 'restore-env-variables',
-      testMatch: /global\.teardown\.ts/,
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-      dependencies: ['set-env-variables'],
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
     // {
     //   name: 'chromium',
@@ -83,9 +73,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
+    command: "npm run dev",
+    url: "http://localhost:3000",
     reuseExistingServer: false,
   },
-  reportSlowTests: null
+  reportSlowTests: null,
 });
