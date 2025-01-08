85d35996704117ad731de6c0555c61f7bbc9454a
Cory Bass <cory@Corys-MacBook-Pro-2.local>
2025-01-08 12:05:50 -0600

    test commit for non pr pushes

--- playwright/e2e/sample-reports-navigation.spec.ts
+++ playwright/e2e/sample-reports-navigation.spec.ts
@@ -1,67 +1,65 @@
 import test, { expect } from "@playwright/test";
 
 test.describe("Sample reports navigation", () => {
-  test.setTimeout(120000);
   test.describe("On 'Release end dates with initiative status' click", () => {
     test.beforeEach(async ({ page }) => {
       await page.goto("/");
-      const releaseLink = page.getByTestId("release-status");
+      const releaseLink = page.getByText("Release end dates with initiative status");
       await releaseLink.waitFor({ state: "visible" });
-      console.log("releaseLink HTML", await releaseLink.innerHTML());
       await releaseLink.click();
     });
 
     test("URL contains primaryIssueType=Release", async ({ page }) => {
       expect(page.url()).toContain("primaryIssueType=Release");
     });
-    // test("URL contains hideUnknownInitiatives=true", async ({ page }) => {
-    //   expect(page.url()).toContain("hideUnknownInitiatives=true");
-    // });
-    // test("URL contains primaryReportType=due", async ({ page }) => {
-    //   expect(page.url()).toContain("primaryReportType=due");
-    // });
-    // test("URL contains secondaryReportType=status", async ({ page }) => {
-    //   expect(page.url()).toContain("secondaryReportType=status");
-    // });
+    test("URL contains hideUnknownInitiatives=true", async ({ page }) => {
+      expect(page.url()).toContain("hideUnknownInitiatives=true");
+    });
+    test("URL contains primaryReportType=due", async ({ page }) => {
+      expect(page.url()).toContain("primaryReportType=due");
+    });
+    test("URL contains secondaryReportType=status", async ({ page }) => {
+      expect(page.url()).toContain("secondaryReportType=status");
+    });
   });
 
-  //   test.describe("On 'Release timeline with initiative work breakdown' click", () => {
-  //     test.beforeEach(async ({ page }) => {
-  //       await page.goto("/");
-  //       await page.getByTestId("release-work-breakdown").click();
-  //     });
+  test.describe("On 'Release timeline with initiative work breakdown' click", () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto("/");
+      await page.getByText("Release timeline with initiative work breakdown").click();
+    });
 
-  //     test("URL contains primaryIssueType=Release", async ({ page }) => {
-  //       expect(page.url()).toContain("primaryIssueType=Release");
-  //     });
-  //     test("URL contains hideUnknownInitiatives=true", async ({ page }) => {
-  //       expect(page.url()).toContain("hideUnknownInitiatives=true");
-  //     });
+    test("URL contains primaryIssueType=Release", async ({ page }) => {
+      expect(page.url()).toContain("primaryIssueType=Release");
+    });
+    test("URL contains hideUnknownInitiatives=true", async ({ page }) => {
+      expect(page.url()).toContain("hideUnknownInitiatives=true");
+    });
 
-  //     test("URL contains secondaryReportType=breakdown", async ({ page }) => {
-  //       expect(page.url()).toContain("secondaryReportType=breakdown");
-  //     });
-  //   });
+    test("URL contains secondaryReportType=breakdown", async ({ page }) => {
+      expect(page.url()).toContain("secondaryReportType=breakdown");
+    });
+  });
 
-  //   test.describe("On 'Ready and in-development initiative work breakdown' click", () => {
-  //     test.beforeEach(async ({ page }) => {
-  //       await page.goto("/");
-  //       await page.getByTestId("initiative-work-breakdown").click();
-  //     });
+  test.describe("On 'Ready and in-development initiative work breakdown' click", () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto("/");
+      await page.getByText("Ready and in-development initiative work breakdown").click();
+    });
 
-  //     test("URL contains primaryIssueType=Initiative", async ({ page }) => {
-  //       expect(page.url()).toContain("primaryIssueType=Initiative");
-  //     });
-  //     test("URL contains hideUnknownInitiatives=true", async ({ page }) => {
-  //       expect(page.url()).toContain("hideUnknownInitiatives=true");
-  //     });
+    test("URL contains primaryIssueType=Initiative", async ({ page }) => {
+      expect(page.url()).toContain("primaryIssueType=Initiative");
+    });
+    test("URL contains hideUnknownInitiatives=true", async ({ page }) => {
+      expect(page.url()).toContain("hideUnknownInitiatives=true");
+    });
 
-  //     test("URL contains primaryReportType=start-due", async ({ page }) => {
-  //       expect(page.url()).toContain("primaryReportType=start-due");
-  //     });
+    test("URL contains primaryReportType=start-due", async ({ page }) => {
+      expect(page.url()).toContain("primaryReportType=start-due");
+    });
 
-  //     test("URL contains primaryReportBreakdown=true", async ({ page }) => {
-  //       expect(page.url()).toContain("primaryReportBreakdown=true");
-  //     });
-  //   });
+    test("URL contains primaryReportBreakdown=true", async ({ page }) => {
+      expect(page.url()).toContain("primaryReportBreakdown=true");
+    });
+  });
 });
