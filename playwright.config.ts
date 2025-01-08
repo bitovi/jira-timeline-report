d6fd30b300c75828a3051171a4297c5977b3f50c
Cory Bass <cory@Corys-MacBook-Pro-2.local>
2025-01-08 10:50:32 -0600

    changed playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "npm run start-local",
     url: "http://localhost:3000",
     reuseExistingServer: false,
   },
