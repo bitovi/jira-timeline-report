6cee5e00dc9a9317398f3c8fa7e19503e548d5ab
David Nicholas <dnicholas@bitovi.com>
2025-01-21 15:39:31 -0800

    routing data to static props

--- public/canjs/controls/select-issue-type/select-issue-type.js
+++ public/canjs/controls/select-issue-type/select-issue-type.js
@@ -12,24 +12,22 @@ const TOOLTIP = new SimpleTooltip();
 document.body.append(TOOLTIP);
 
 const booleanParsing = {
-    parse: x => {
-      return ({"": true, "true": true, "false": false})[x];
-    },
-    stringify: x => ""+x
-  };
+  parse: (x) => {
+    return { "": true, true: true, false: false }[x];
+  },
+  stringify: (x) => "" + x,
+};
 
+const selectStyle =
+  "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
 
-const selectStyle = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-
-const hoverEffect = "hover:bg-neutral-301 cursor-pointer"
-
-
+const hoverEffect = "hover:bg-neutral-301 cursor-pointer";
 
 const RELEASES_TOOLTIP = new SimpleTooltip();
 document.body.append(RELEASES_TOOLTIP);
 
 class TypeSelectionDropdown extends StacheElement {
-    static view = `
+  static view = `
         {{# for(issueType of this.issueHierarchy) }}
         <label class="px-4 py-2 block {{#eq(this.primaryIssueType, issueType.name)}}bg-blue-101{{else}}${hoverEffect}{{/eq}}"><input 
             type="radio" 
@@ -41,22 +39,30 @@ class TypeSelectionDropdown extends StacheElement {
             on:mouseenter="this.showReleases(scope.element)">
             Releases <img class="inline" src="/images/chevron-right-new.svg"/> 
         </label>
-    `
-    showReleases(label) {
-        let dropdown = new ReleasesTypeSelectionDropdown().initialize({
-            issueHierarchy: this.issueHierarchy,
-            onSelection: this.onSelection,
-            secondaryIssueType: this.routeData.secondaryIssueType
-        })
+    `;
 
-        RELEASES_TOOLTIP.rightOfElementInScrollingContainer(label, dropdown);
-    }
+  static props = {
+    routeData: {
+      get default() {
+        return routeData;
+      },
+    },
+  };
+
+  showReleases(label) {
+    let dropdown = new ReleasesTypeSelectionDropdown().initialize({
+      issueHierarchy: this.issueHierarchy,
+      onSelection: this.onSelection,
+      secondaryIssueType: this.routeData.secondaryIssueType,
+    });
+
+    RELEASES_TOOLTIP.rightOfElementInScrollingContainer(label, dropdown);
+  }
 }
 customElements.define("select-type-dropdown", TypeSelectionDropdown);
 
-
 class ReleasesTypeSelectionDropdown extends StacheElement {
-    static view = `
+  static view = `
         {{# for(issueType of this.issueHierarchy) }}
         <label class="px-4 py-2 block {{#eq(this.secondaryIssueType, issueType.name)}}bg-blue-101{{else}}${hoverEffect}{{/eq}}"><input 
             type="radio" 
@@ -64,15 +70,12 @@ class ReleasesTypeSelectionDropdown extends StacheElement {
             checked:from="eq(this.secondaryIssueType, issueType.name)"
             on:change="this.onSelection('Release', issueType.name)"/> {{issueType.name}}s </label>
         {{/ }}
-    `
+    `;
 }
 customElements.define("select-release-type-dropdown", ReleasesTypeSelectionDropdown);
 
-
-
-
 export class SelectIssueType extends StacheElement {
-    static view = `
+  static view = `
         <label for="reportOn" class="${DROPDOWN_LABEL}">Report on</label>
         {{# not(this.routeData.primaryIssueType) }}
             <button class="rounded bg-neutral-201 px-3 py-1" id="reportOn">Loading ... </button>
@@ -87,45 +90,45 @@ export class SelectIssueType extends StacheElement {
             </button>
         {{/ }}
     `;
-    static props ={     
-        routeData: {
-          get default() { return routeData; }
-        },
-        get issueHierarchy(){
-            return this.derivedIssues && this.derivedIssues.length ?
-                issueHierarchyFromNormalizedIssues(this.derivedIssues) :
-                this.simplifiedIssueHierarchy;
-            
-        }
-    };
-    onSelection(primaryType, secondaryType){
-        if(secondaryType) {
-            this.routeData.selectedIssueType = "Release-"+secondaryType;
-        } else {
-            this.routeData.selectedIssueType = primaryType;
-        }
+  static props = {
+    routeData: {
+      get default() {
+        return routeData;
+      },
+    },
+    get issueHierarchy() {
+      return this.derivedIssues && this.derivedIssues.length
+        ? issueHierarchyFromNormalizedIssues(this.derivedIssues)
+        : this.simplifiedIssueHierarchy;
+    },
+  };
+  onSelection(primaryType, secondaryType) {
+    if (secondaryType) {
+      this.routeData.selectedIssueType = "Release-" + secondaryType;
+    } else {
+      this.routeData.selectedIssueType = primaryType;
+    }
+    TOOLTIP.leftElement();
+    RELEASES_TOOLTIP.leftElement();
+  }
+  showChildOptions() {
+    let dropdown = new TypeSelectionDropdown().initialize({
+      primaryIssueType: this.routeData.primaryIssueType,
+      secondaryIssueType: this.routeData.secondaryIssueType,
+      issueHierarchy: this.issueHierarchy,
+      onSelection: this.onSelection.bind(this),
+    });
+
+    TOOLTIP.belowElementInScrollingContainer(this, dropdown);
+  }
+  connected() {
+    this.listenTo(window, "click", (event) => {
+      if (!TOOLTIP.contains(event.target)) {
         TOOLTIP.leftElement();
         RELEASES_TOOLTIP.leftElement();
-    }
-    showChildOptions(){
-        let dropdown = new TypeSelectionDropdown().initialize({
-            primaryIssueType: this.routeData.primaryIssueType,
-            secondaryIssueType: this.routeData.secondaryIssueType,
-            issueHierarchy: this.issueHierarchy,
-            onSelection: this.onSelection.bind(this)
-        })
-
-        TOOLTIP.belowElementInScrollingContainer(this, dropdown);
-    }
-    connected(){
-        this.listenTo(window, "click", (event)=>{
-          if(!TOOLTIP.contains(event.target))   {
-            TOOLTIP.leftElement();
-            RELEASES_TOOLTIP.leftElement();
-          }
-        })
-    }
+      }
+    });
+  }
 }
-
 
 customElements.define("select-issue-type", SelectIssueType);
