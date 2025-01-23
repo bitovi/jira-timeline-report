b57a5bf9572b94717d3ba91a1d079ef7003ae955
David Nicholas <dnicholas@bitovi.com>
2025-01-23 13:31:03 -0800

    remove dbl debounce

--- public/react/services/theme/useSaveTheme.tsx
+++ public/react/services/theme/useSaveTheme.tsx
@@ -12,23 +12,6 @@ import { themeKeys } from "./key-factory";
 
 import { useState, useEffect } from "react";
 
-function useDebounce<T>(value: T, delay: number): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value);
-
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-
-    // Cleanup function to cancel the previous timeout if value changes
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [value, delay]);
-
-  return debouncedValue;
-}
-
 export const useSaveTheme = () => {
   const storage = useStorage();
   const queryClient = useQueryClient();
