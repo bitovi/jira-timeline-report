b35e2fd20adbf697da2a9cfaecff9bd7101a7ebb
David Nicholas <dnicholas@bitovi.com>
2025-01-30 14:16:44 -0800

    update dsn

--- public/shared/sentry.js
+++ public/shared/sentry.js
@@ -1,11 +1,11 @@
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://c8f8aad7cbf776a99570cd163a5c86b1@o4508721931616256.ingest.us.sentry.io/4508733956947968",
+  dsn: process.env.FRONTEND_SENTRY_DSN,
   integrations: [Sentry.browserTracingIntegration()],
   tracesSampleRate: 1.0,
-  enabled: process.env.NODE_ENV === "production",
-  environment: process.env.CLIENT_STATUS_REPORTS_ENV,
+  enabled: !!process.env.FRONTEND_SENTRY_DSN,
+  environment: process.env.STATUS_REPORTS_ENV,
 });
 
 window.addEventListener("error", (error) => Sentry.captureException(error));
--- server/instruments.js
+++ server/instruments.js
@@ -2,10 +2,10 @@ const Sentry = require("@sentry/node");
 const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 
 Sentry.init({
-  dsn: "https://26de5611d04fde803427885bdb82a7bc@o4508721931616256.ingest.us.sentry.io/4508721935351808",
+  dsn: process.env.BACKEND_SENTRY_DSN,
   integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1.0,
-  enabled: process.env.NODE_ENV === "production",
+  enabled: !!process.env.BACKEND_SENTRY_DSN,
   environment: process.env.CLIENT_STATUS_REPORTS_ENV,
 });
 
