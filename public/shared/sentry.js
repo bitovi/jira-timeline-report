c12a8eb1e9483fffa2023d4c6d3aab53c4949104
David Nicholas <dnicholas@bitovi.com>
2025-01-30 15:25:43 -0800

    fix init

--- public/shared/sentry.js
+++ public/shared/sentry.js
@@ -2,11 +2,11 @@ import * as Sentry from "@sentry/react";
 
 export const initSentry = ({ FRONTEND_SENTRY_DSN, STATUS_REPORTS_ENV }) => {
   Sentry.init({
-    dsn: process.env.FRONTEND_SENTRY_DSN,
+    dsn: FRONTEND_SENTRY_DSN,
     integrations: [Sentry.browserTracingIntegration()],
     tracesSampleRate: 1.0,
-    enabled: !!process.env.FRONTEND_SENTRY_DSN,
-    environment: process.env.STATUS_REPORTS_ENV,
+    enabled: !!FRONTEND_SENTRY_DSN,
+    environment: STATUS_REPORTS_ENV,
   });
 
   window.addEventListener("error", (error) => Sentry.captureException(error));
