b8caeef4aa3c0be2197e97b64a8ff3ae2342eaeb
Cherif Bouchelaghem <cherif@bitovi.com>
2025-02-04 13:48:40 -0500

    TR-193 E2E tests for redering links pages correctly

--- playwright/e2e/sample-reports-navigation.spec.ts
+++ playwright/e2e/sample-reports-navigation.spec.ts
@@ -21,6 +21,21 @@ test.describe("Sample reports navigation", () => {
     test("URL contains secondaryReportType=status", async ({ page }) => {
       expect(page.url()).toContain("secondaryReportType=status");
     });
+
+    test("the page contains existing initiatives", async ({ page }) => {
+      const reportOnBtn = page.locator('select-issue-type').locator('button');
+      const reportTypeBtn = page.locator('select-report-type').locator('button');
+      await expect(reportOnBtn).toHaveText('Initiatives');
+      await expect(reportTypeBtn).toHaveText('Scatter Plot');
+      const initiatives = await page.locator('.release-timeline-item').count();
+      await expect(initiatives).toBeGreaterThan(0)
+    });
+
+    test("the page has status report", async ({ page }) => {
+      await expect(page.locator("status-report")).toBeVisible();
+      const items = await page.locator("status-report .release_box ul li").count();
+      await expect(items).toBeGreaterThan(1);
+    });
   });
 
   test.describe("On 'Release timeline with initiative work breakdown' click", () => {
@@ -39,6 +54,20 @@ test.describe("Sample reports navigation", () => {
     test("URL contains secondaryReportType=breakdown", async ({ page }) => {
       expect(page.url()).toContain("secondaryReportType=breakdown");
     });
+
+    test("the page contains existing initiatives", async ({ page }) => {
+      const reportOnBtn = page.locator('select-issue-type').locator('button');
+      const reportTypeBtn = page.locator('select-report-type').locator('button');
+      await expect(reportOnBtn).toHaveText('Initiatives');
+      await expect(reportTypeBtn).toHaveText('Gantt Chart');
+      await expect(page.locator('gantt-grid')).toBeVisible()
+    });
+
+    test("the page has status report", async ({ page }) => {
+      await expect(page.locator("status-report")).toBeVisible();
+      const items = await page.locator("status-report .release_box ul li").count();
+      await expect(items).toBeGreaterThan(1);
+    })
   });
 
   test.describe("On 'Ready and in-development initiative work breakdown' click", () => {
@@ -61,5 +90,14 @@ test.describe("Sample reports navigation", () => {
     test("URL contains primaryReportBreakdown=true", async ({ page }) => {
       expect(page.url()).toContain("primaryReportBreakdown=true");
     });
+
+    test("the page contains existing initiatives", async ({ page }) => {
+      const reportOnBtn = page.locator('select-issue-type').locator('button');
+      const reportTypeBtn = page.locator('select-report-type').locator('button');
+      await expect(reportOnBtn).toHaveText('Initiatives');
+      await expect(reportTypeBtn).toHaveText('Gantt Chart');
+      await expect(page.locator('gantt-grid')).toBeVisible()
+    });
+
   });
 });
