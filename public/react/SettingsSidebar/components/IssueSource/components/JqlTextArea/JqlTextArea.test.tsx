d1a4d62b2ccb31d78140fdae7522a615eb8c9b98
David Nicholas <dnicholas@bitovi.com>
2025-02-06 12:31:22 -0800

    smoke tests

---
+++ public/react/SettingsSidebar/components/IssueSource/components/JqlTextArea/JqlTextArea.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import { vi } from "vitest";
+
+import JQLTextArea from "./JqlTextArea";
+
+describe("<JQLTextArea />", () => {
+  it("renders without crashing", () => {
+    render(
+      <JQLTextArea
+        jql="issueType in (Epic, Story) order by Rank"
+        setJql={vi.fn()}
+        isLoading
+        isSuccess={false}
+        totalChunks={10}
+        receivedChunks={5}
+        numberOfIssues={10}
+      />
+    );
+
+    const jqlTextarea = screen.getByLabelText("Add your JQL");
+    expect(jqlTextarea).toBeInTheDocument();
+
+    const issuesLoadedText = screen.getByText("Loaded 5 of 10 issues");
+    expect(issuesLoadedText).toBeInTheDocument();
+  });
+});
