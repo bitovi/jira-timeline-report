b1008a27b93f714edd4cfe8e7f5fcd974603decb
David Nicholas <dnicholas@bitovi.com>
2025-02-10 16:49:03 -0800

    initial features test

---
+++ public/react/SettingsSidebar/components/Features/components/FeatureToggle/FeatureToggle.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import FeatureToggle from "./FeatureToggle";
+
+describe("<FeatureToggle />", () => {
+  it("renders without crashing", () => {
+    render(<FeatureToggle title="Test Feature" subtitle="Description of the feature" />);
+
+    const title = screen.getByText("Test Feature");
+    expect(title).toBeInTheDocument();
+
+    const subtitle = screen.getByText("Description of the feature");
+    expect(subtitle).toBeInTheDocument();
+  });
+});
