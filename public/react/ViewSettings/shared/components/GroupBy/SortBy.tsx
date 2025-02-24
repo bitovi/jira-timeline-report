97b27116bcd9567719d2c4c229ae67eb77edefcb
David Nicholas <dnicholas@bitovi.com>
2025-02-24 14:07:58 -0800

    sort by

--- public/react/ViewSettings/shared/components/GroupBy/SortBy.tsx
+++ public/react/ViewSettings/shared/components/GroupBy/SortBy.tsx
@@ -3,9 +3,40 @@ import type { FC } from "react";
 import React, { useId } from "react";
 import Select from "@atlaskit/select";
 import VisuallyHidden from "@atlaskit/visually-hidden";
+import { useCanObservable } from "../../../../hooks/useCanObservable";
+import { value } from "../../../../../can";
+import routeData from "../../../../../canjs/routing/route-data";
+
+const booleanParsing = {
+  parse: (x: string) => {
+    return { "": true, true: true, false: false }[x];
+  },
+  stringify: (x: boolean) => "" + x,
+};
+
+const useSortBy = () => {
+  const sortBy = [
+    { label: "JQL Order", value: "false" },
+    { label: "Due Date", value: "true" },
+  ];
+
+  const selectedSortBy = useCanObservable<boolean>(value.from(routeData, "sortByDueDate"));
+
+  const setSelectedSortBy = (value: string) => {
+    // @ts-expect-error
+    routeData.sortByDueDate = booleanParsing.parse(value);
+  };
+
+  return {
+    sortBy,
+    selectedSortBy: sortBy.find(({ value }) => value === booleanParsing.stringify(selectedSortBy)),
+    setSelectedSortBy,
+  };
+};
 
 const SortBy: FC = () => {
   const id = useId();
+  const { sortBy, selectedSortBy, setSelectedSortBy } = useSortBy();
 
   return (
     <div className="flex items-center gap-2">
@@ -13,13 +44,11 @@ const SortBy: FC = () => {
         <label htmlFor={id}>Sort By</label>
       </VisuallyHidden>
       <Select
-        isMulti
-        isSearchable
         id={id}
         className="flex-1"
-        options={[]}
-        value={undefined}
-        // onChange={setSelectedStatus}
+        options={sortBy}
+        value={selectedSortBy}
+        onChange={(option) => setSelectedSortBy(option?.value ?? "false")}
       />
     </div>
   );
