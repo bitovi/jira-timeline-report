9221913797661d0e3a443d612b9d9ee76ab9f60f
David Nicholas <dnicholas@bitovi.com>
2025-02-24 14:51:21 -0800

    secondary report type

--- public/react/ViewSettings/shared/components/RoundDatesTo/RoundDatesTo.tsx
+++ public/react/ViewSettings/shared/components/RoundDatesTo/RoundDatesTo.tsx
@@ -3,9 +3,36 @@ import type { FC } from "react";
 import React, { useId } from "react";
 import Select from "@atlaskit/select";
 import VisuallyHidden from "@atlaskit/visually-hidden";
+import { useCanObservable } from "../../../../hooks/useCanObservable";
+import routeData from "../../../../../canjs/routing/route-data";
+import { value } from "../../../../../can";
+
+const useRoundDatesTo = () => {
+  const roundDatesTo = [
+    { value: "day", label: "Day" },
+    { value: "week", label: "Week" },
+    { value: "month", label: "Month" },
+    { value: "halfQuarter", label: "Half Quarter" },
+    { value: "quarter", label: "Quarter" },
+  ];
+
+  const selectedRoundDatesTo = useCanObservable<string>(value.from(routeData, "roundTo"));
+
+  const setSelectedRoundDatesTo = (value: string) => {
+    // @ts-expect-error
+    routeData.roundTo = value;
+  };
+
+  return {
+    roundDatesTo,
+    selectedRoundDatesTo: roundDatesTo.find(({ value }) => value === selectedRoundDatesTo),
+    setSelectedRoundDatesTo,
+  };
+};
 
 const RoundDatesTo: FC = () => {
   const id = useId();
+  const { roundDatesTo, selectedRoundDatesTo, setSelectedRoundDatesTo } = useRoundDatesTo();
 
   return (
     <div className="flex items-center gap-2">
@@ -13,13 +40,11 @@ const RoundDatesTo: FC = () => {
         <label htmlFor={id}>Round Dates To</label>
       </VisuallyHidden>
       <Select
-        isMulti
-        isSearchable
         id={id}
         className="flex-1"
-        options={[]}
-        value={undefined}
-        // onChange={setSelectedStatus}
+        options={roundDatesTo}
+        value={selectedRoundDatesTo}
+        onChange={(option) => setSelectedRoundDatesTo(option?.value ?? "")}
       />
     </div>
   );
