c50456a199fbc51d88f33e360901e559f6a0afd9
David Nicholas <dnicholas@bitovi.com>
2025-02-24 14:24:55 -0800

    group by

---
+++ public/react/ViewSettings/shared/components/GroupBy/GroupBy.tsx
@@ -0,0 +1,51 @@
+import type { FC } from "react";
+
+import React, { useId } from "react";
+import Select from "@atlaskit/select";
+import VisuallyHidden from "@atlaskit/visually-hidden";
+import { useCanObservable } from "../../../../hooks/useCanObservable";
+import { value } from "../../../../../can";
+import routeData from "../../../../../canjs/routing/route-data";
+
+const useGroupBy = () => {
+  const groupBy = [
+    { label: "None", value: "" },
+    { label: "Parent", value: "parent" },
+    { label: "Team", value: "team" },
+  ];
+
+  const selectedGroupBy = useCanObservable<string>(value.from(routeData, "groupBy"));
+
+  const setSelectedGroupBy = (value: string) => {
+    // @ts-expect-error
+    routeData.groupBy = value;
+  };
+
+  return {
+    groupBy,
+    selectedGroupBy: groupBy.find(({ value }) => value === selectedGroupBy),
+    setSelectedGroupBy,
+  };
+};
+
+const GroupBy: FC = () => {
+  const id = useId();
+  const { groupBy, selectedGroupBy, setSelectedGroupBy } = useGroupBy();
+
+  return (
+    <div className="flex items-center gap-2">
+      <VisuallyHidden>
+        <label htmlFor={id}>Group By</label>
+      </VisuallyHidden>
+      <Select
+        id={id}
+        className="flex-1"
+        options={groupBy}
+        value={selectedGroupBy}
+        onChange={(option) => setSelectedGroupBy(option?.value ?? "")}
+      />
+    </div>
+  );
+};
+
+export default GroupBy;
--- public/react/ViewSettings/shared/components/SortBy/index.ts
+++ public/react/ViewSettings/shared/components/SortBy/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./GroupBy";
+export { default } from "./SortBy";
