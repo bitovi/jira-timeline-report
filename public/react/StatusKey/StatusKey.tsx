a1bf3864b7839ec91ae543396b571027cfabf76c
David Nicholas <dnicholas@bitovi.com>
2025-02-25 13:28:19 -0800

    status key reorg

--- public/react/StatusKey/StatusKey.tsx
+++ public/react/StatusKey/StatusKey.tsx
@@ -2,26 +2,17 @@ import React, { FC, ReactNode } from "react";
 import Lozenge from "@atlaskit/lozenge";
 import { useTheme } from "../services/theme";
 
-const StatusKeyItem: FC<{ icon: ReactNode; children: ReactNode }> = ({ icon, children }) => (
-  <div className="flex space-x-2 content-center">
-    <div className="color-text-and-bg-notstarted text-xs w-4 h-4 relative flex content-center justify-center rounded-full">
-      {icon}
-    </div>
-    <div className="color-text-notstarted text-xs">{children}</div>
-  </div>
-);
-
 const StatusKey: FC = () => {
   const theme = useTheme();
 
   return (
     <div className="flex flex-wrap gap-x-4 gap-y-3 p-2 justify-between">
       <div className="flex gap-x-8">
-        <StatusKeyItem icon={<img className="m-0.5" src="/images/empty-set.svg" />}>
+        <ItemStatus icon={<img className="m-0.5" src="/images/empty-set.svg" />}>
           Unknown dates
-        </StatusKeyItem>
-        <StatusKeyItem icon="←">Dates are in the past outside this view</StatusKeyItem>
-        <StatusKeyItem icon="→">Dates are in the future outside this view</StatusKeyItem>
+        </ItemStatus>
+        <ItemStatus icon="←">Dates are in the past outside this view</ItemStatus>
+        <ItemStatus icon="→">Dates are in the future outside this view</ItemStatus>
       </div>
       <div className="flex gap-x-1">
         {theme.map(({ backgroundCssVar, textCssVar, label }) => (
@@ -41,3 +32,19 @@ const StatusKey: FC = () => {
 };
 
 export default StatusKey;
+
+interface ItemStatusProps {
+  icon: ReactNode;
+  children: ReactNode;
+}
+
+const ItemStatus: FC<ItemStatusProps> = ({ icon, children }) => {
+  return (
+    <div className="flex space-x-2 content-center">
+      <div className="color-text-and-bg-notstarted text-xs w-4 h-4 relative flex content-center justify-center rounded-full">
+        {icon}
+      </div>
+      <div className="color-text-notstarted text-xs">{children}</div>
+    </div>
+  );
+};
