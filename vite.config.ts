314b0f532ea801c2a53284784a019407aa846c32
David Nicholas <dnicholas@bitovi.com>
2025-04-08 12:28:02 -0700

    split

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
-import { defineConfig } from "vitest/config";
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "/index.html"),
+        oauth: resolve(__dirname, "/oauth-callback.html"),
+        connect: resolve(__dirname, "/connect.html"),
+        // dev: resolve(__dirname, "/dev.html"),
+      },
+    },
+  },
   test: {
     environment: "jsdom",
     globalSetup: "./vitest-global.ts",
