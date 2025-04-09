3834b84f9ce9f1183456037dccd5dbd34651424d
David Nicholas <dnicholas@bitovi.com>
2025-04-09 14:51:14 -0700

    dev build

---
+++ vite.dev.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    emptyOutDir: false,
+    minify: false,
+    sourcemap: "inline",
+    rollupOptions: {
+      input: {
+        dev: resolve(__dirname, "/dev.html"),
+      },
+    },
+  },
+});
