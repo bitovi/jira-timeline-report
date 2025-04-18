1124fe4c5c1b2e9120998d50528ce37c5b2152b5
David Nicholas <dnicholas@bitovi.com>
2025-04-18 12:44:32 -0700

    feature request form

--- src/shared/sentry.js
+++ src/shared/sentry.js
@@ -3,11 +3,13 @@ import * as Sentry from "@sentry/react";
 export const initSentry = ({ FRONTEND_SENTRY_DSN, STATUS_REPORTS_ENV }) => {
   Sentry.init({
     dsn: FRONTEND_SENTRY_DSN,
-    integrations: [Sentry.browserTracingIntegration()],
+    integrations: [Sentry.browserTracingIntegration(), Sentry.feedbackIntegration()],
     tracesSampleRate: 1.0,
     enabled: !!FRONTEND_SENTRY_DSN,
     environment: STATUS_REPORTS_ENV,
   });
+
+  console.log({ enabled: !!FRONTEND_SENTRY_DSN });
 
   window.addEventListener("error", (error) => Sentry.captureException(error));
 };
