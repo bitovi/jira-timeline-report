84995d408a53188cce36d6a7a3f6b4384364f3a6
David Nicholas <dnicholas@bitovi.com>
2025-04-24 09:46:43 -0700

    add bug modal

--- src/react/SettingsSidebar/components/ReportSettings/components/BugReportModal/components/BugReportForm/BugReportForm.test.tsx
+++ src/react/SettingsSidebar/components/ReportSettings/components/BugReportModal/components/BugReportForm/BugReportForm.test.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import { vi, describe, expect, it } from "vitest";
 
-import FeatureRequestForm from "./FeatureRequestForm";
+import BugReportForm from "./BugReportForm";
 import { FlagsProvider } from "@atlaskit/flag";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import userEvent from "@testing-library/user-event";
 
-describe("<FeatureRequestForm />", () => {
+describe("<BugReportForm />", () => {
   it("renders", () => {
     const mockOnCancel = vi.fn();
     const mockOnSubmit = vi.fn();
     const client = new QueryClient();
 
-    render(<FeatureRequestForm onCancel={mockOnCancel} onSubmit={mockOnSubmit} />, {
+    render(<BugReportForm onCancel={mockOnCancel} onSubmit={mockOnSubmit} />, {
       wrapper: ({ children }) => (
         <FlagsProvider>
           <QueryClientProvider client={client}>{children}</QueryClientProvider>
@@ -21,12 +21,13 @@ describe("<FeatureRequestForm />", () => {
       ),
     });
 
-    const emailLabel = screen.getByLabelText(
-      /if you're open to follow-up questions, what's your email\?/i
-    );
+    const nameLabel = screen.getByLabelText(/name/i);
+    expect(nameLabel).toBeInTheDocument();
+
+    const emailLabel = screen.getByLabelText(/email/i);
     expect(emailLabel).toBeInTheDocument();
 
-    const descriptionLabel = screen.getByLabelText(/what are you trying to report on\?/i);
+    const descriptionLabel = screen.getByLabelText(/description/i);
     expect(descriptionLabel).toBeInTheDocument();
 
     const dropzoneLabel = screen.getByLabelText(/Drag and drop files or click to upload/i);
@@ -39,13 +40,13 @@ describe("<FeatureRequestForm />", () => {
     expect(cancelButton).toBeInTheDocument();
   });
 
-  describe("FeatureRequestForm Component", () => {
+  describe("BugReportForm Component", () => {
     it("calls onCancel when the Cancel button is clicked", async () => {
       const mockOnCancel = vi.fn();
       const mockOnSubmit = vi.fn();
       const client = new QueryClient();
 
-      render(<FeatureRequestForm onCancel={mockOnCancel} onSubmit={mockOnSubmit} />, {
+      render(<BugReportForm onCancel={mockOnCancel} onSubmit={mockOnSubmit} />, {
         wrapper: ({ children }) => (
           <FlagsProvider>
             <QueryClientProvider client={client}>{children}</QueryClientProvider>
@@ -65,7 +66,7 @@ describe("<FeatureRequestForm />", () => {
     const mockOnSubmit = vi.fn();
     const client = new QueryClient();
 
-    render(<FeatureRequestForm onCancel={mockOnCancel} onSubmit={mockOnSubmit} />, {
+    render(<BugReportForm onCancel={mockOnCancel} onSubmit={mockOnSubmit} />, {
       wrapper: ({ children }) => (
         <FlagsProvider>
           <QueryClientProvider client={client}>{children}</QueryClientProvider>
@@ -76,16 +77,16 @@ describe("<FeatureRequestForm />", () => {
     const submitButton = screen.getByRole("button", { name: /submit/i });
     await userEvent.click(submitButton);
 
-    const errorMessage = await screen.findByText(/you need to add a feature description/i);
+    const errorMessage = await screen.findByText(/you need to add a description/i);
     expect(errorMessage).toBeInTheDocument();
   });
 
-  it("submits the form with description and email", async () => {
+  it("submits the form with name, description and email", async () => {
     const mockOnCancel = vi.fn();
     const mockOnSubmit = vi.fn();
     const client = new QueryClient();
 
-    render(<FeatureRequestForm onCancel={mockOnCancel} onSubmit={mockOnSubmit} />, {
+    render(<BugReportForm onCancel={mockOnCancel} onSubmit={mockOnSubmit} />, {
       wrapper: ({ children }) => (
         <FlagsProvider>
           <QueryClientProvider client={client}>{children}</QueryClientProvider>
@@ -93,13 +94,15 @@ describe("<FeatureRequestForm />", () => {
       ),
     });
 
-    const emailInput = screen.getByLabelText(
-      /if you're open to follow-up questions, what's your email\?/i
-    );
+    const nameInput = screen.getByLabelText(/name/i);
+
+    await userEvent.type(nameInput, "test");
+
+    const emailInput = screen.getByLabelText(/email/i);
 
     await userEvent.type(emailInput, "test@example.com");
 
-    const descriptionTextArea = screen.getByLabelText(/what are you trying to report on\?/i);
+    const descriptionTextArea = screen.getByLabelText(/description/i);
     await userEvent.type(descriptionTextArea, "I want a weekly breakdown");
 
     const submitButton = screen.getByRole("button", { name: /submit/i });
