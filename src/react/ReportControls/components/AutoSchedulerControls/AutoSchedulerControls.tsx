82c44d401dd02221b50d3dd89bc263685e38c6f1
Mark Thompson <mthompson@bitovi.com>
2025-06-03 12:21:27 -0700

    style changes

--- src/react/ReportControls/components/AutoSchedulerControls/AutoSchedulerControls.tsx
+++ src/react/ReportControls/components/AutoSchedulerControls/AutoSchedulerControls.tsx
@@ -7,6 +7,7 @@ import { useUncertaintyWeight } from '../../../hooks/useUncertaintyWeight';
 import { useSelectedStartDate } from '../../../hooks/useSelectedStartDate/useSelectedStartDate';
 import { useSelectedIssueType } from '../../../services/issues';
 import { useRouteData } from '../../../hooks/useRouteData';
+import { Label } from '@atlaskit/form';
 
 const AutoSchedulerControls: FC = () => {
   const [selectedStartDate, setSelectedStartDate] = useSelectedStartDate();
@@ -17,17 +18,23 @@ const AutoSchedulerControls: FC = () => {
   return (
     <>
       <UncertaintySlider uncertaintyWeight={uncertaintyWeight} onChange={setUncertaintyWeight} />
-      <label htmlFor="date-picker">Start Date:</label>
-      <input
-        className="border border-gray-300 rounded-md px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 dark:bg-white/10 dark:text-white"
-        type="date"
-        // format for input
-        value={selectedStartDate.toISOString().split('T')[0]}
-        onChange={(e) => setSelectedStartDate(e.target.valueAsDate)}
-      />
-      <Button appearance="primary" onClick={() => setIsOpen(true)}>
-        Update {selectedIssueType} Dates
-      </Button>
+      <div className="flex flex-col items-start pt-1 pl-1">
+        <Label htmlFor="auto-scheduler-date-picker">Start date</Label>
+        <input
+          id="auto-scheduler-date-picker"
+          className="border border-gray-300 rounded-md px-[11px] pr-[7px] py-[5px] text-sm leading-[18px] shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-10 cursor-pointer"
+          type="date"
+          // format for input
+          value={selectedStartDate.toISOString().split('T')[0]}
+          onChange={(e) => setSelectedStartDate(e.target.valueAsDate)}
+        />
+      </div>
+
+      <div className="self-end">
+        <Button appearance="primary" onClick={() => setIsOpen(true)}>
+          Update {selectedIssueType} Dates
+        </Button>
+      </div>
     </>
   );
 };
