a8df260f190e21283a5788a37c7445257c57ebfa
Mark Thompson <mthompson@bitovi.com>
2025-06-03 16:18:05 -0700

    added loading state to gantt chart footer

--- src/react/ReportFooter/components/StatusKey/StatusKeyWrapper.tsx
+++ src/react/ReportFooter/components/StatusKey/StatusKeyWrapper.tsx
@@ -1,21 +1,24 @@
 import type { FC } from 'react';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import { FlagsProvider } from '@atlaskit/flag';
 import { QueryClientProvider } from '@tanstack/react-query';
 import StatusKey from './StatusKey';
 import { StorageProvider } from '../../../services/storage';
 import { queryClient } from '../../../services/query';
+import Skeleton from '../../../components/Skeleton';
 import routeData from '../../../../canjs/routing/route-data';
 
 const StatusKeyWrapper: FC = () => {
   return (
     <FlagsProvider>
-      <QueryClientProvider client={queryClient}>
-        <StorageProvider storage={routeData.storage}>
-          <StatusKey />
-        </StorageProvider>
-      </QueryClientProvider>
+      <Suspense fallback={<Skeleton />}>
+        <QueryClientProvider client={queryClient}>
+          <StorageProvider storage={routeData.storage}>
+            <StatusKey />
+          </StorageProvider>
+        </QueryClientProvider>
+      </Suspense>
     </FlagsProvider>
   );
 };
